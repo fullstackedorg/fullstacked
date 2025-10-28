4a80616be5f8dec7cedab2679875a16974aa4ad9
cplepage <lepage.charlesphilippe@gmail.com>
2025-10-28 14:08:16 -0400

    patched invalid utf8 in tsgo

--- core/build/typescript-go-patch/patch.js
+++ core/build/typescript-go-patch/patch.js
@@ -9,6 +9,22 @@ contents = contents.replace(
 );
 fs.writeFileSync(fileToPatch, contents);
 
+
+// fix invalid utf8 panic
+const fileToPatch2 = "../typescript-go/internal/ls/signaturehelp.go";
+let contents2 = fs.readFileSync(fileToPatch2, { encoding: "utf8" });
+contents2 = contents2
+    .replace(
+        `Label:         displayParts.String(),`,
+        `Label:         strings.ToValidUTF8(displayParts.String(), ""),`
+    )
+    .replace(
+        `Label:         display.String(),`,
+        `Label:         strings.ToValidUTF8(display.String(), ""),`
+    );
+fs.writeFileSync(fileToPatch2, contents2);
+
+
 // put tsgo module into the codebase
 const sourceFile = "./typescript-go-patch/module/tsgo.go";
 const outDir = "../typescript-go/cmd/module";
@@ -16,3 +32,5 @@ const outFile = outDir + "/tsgo.go";
 
 fs.mkdirSync(outDir, { recursive: true });
 fs.cpSync(sourceFile, outFile);
+
+
