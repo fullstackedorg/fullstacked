f557b2e46470b7bb533f6012e846ad11693cb1c9
cplepage <lepage.charlesphilippe@gmail.com>
2025-11-02 09:23:51 -0500

    removed debug auto update pull

--- fullstacked_modules/auto_update.ts
+++ fullstacked_modules/auto_update.ts
@@ -27,7 +27,6 @@ async function checkForUpdates() {
     lastUpdateCheck = now;
 
     const pullResponse = await git.pull();
-    console.log(`Pull Response: [${pullResponse}]`);
     if (pullResponse !== git.PullResponse.DID_PULL) {
         return;
     }
