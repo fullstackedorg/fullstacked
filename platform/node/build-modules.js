2710588dee90150392754a942e13a4a2361dc4aa
cplepage <lepage.charlesphilippe@gmail.com>
2025-11-24 14:57:18 -0500

    files

---
+++ platform/node/build-modules.js
@@ -0,0 +1,24 @@
+import path from "node:path";
+import fs from "node:fs";
+
+await import("../../declarations.js");
+
+const currentDirectory = import.meta.dirname;
+const rootDirectory = path.resolve(currentDirectory, "..", "..")
+
+const fullstackedModulesDirectory = path.resolve(
+    currentDirectory,
+    "fullstacked_modules"
+);
+
+if (fs.existsSync(fullstackedModulesDirectory)) {
+    fs.rmSync(fullstackedModulesDirectory, { recursive: true });
+}
+
+fs.cpSync(
+    path.resolve(rootDirectory, "fullstacked_modules"),
+    fullstackedModulesDirectory,
+    { recursive: true }
+);
+
+(await import("../../build-sass.js")).buildSASS(path.resolve(fullstackedModulesDirectory, "sass", "index.js"));
