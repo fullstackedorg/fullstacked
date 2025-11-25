e0a4fe66ad98f1b7b823b8465ad596767b21a91b
cplepage <lepage.charlesphilippe@gmail.com>
2025-11-25 10:38:50 -0500

    updated typescript-go

--- core/build/typescript-go-patch/patch.js
+++ core/build/typescript-go-patch/patch.js
@@ -7,11 +7,12 @@ const toPatch = [
     {
         // /dev/null/infered crashes everything
         file: path.resolve(tsgoDirectory, "internal", "project", "project.go"),
-        replace: [{
-            from: `inferredProjectName = "/dev/null/inferred"`,
-            to: `inferredProjectName = ".inferred"`
-        }]
-        
+        replace: [
+            {
+                from: `inferredProjectName = "/dev/null/inferred"`,
+                to__: `inferredProjectName = ".inferred"`
+            }
+        ]
     },
     {
         // fix invalid utf8 panic
@@ -19,11 +20,11 @@ const toPatch = [
         replace: [
             {
                 from: `Label:         displayParts.String(),`,
-                to: `Label:         strings.ToValidUTF8(displayParts.String(), ""),`
+                to__: `Label:         strings.ToValidUTF8(displayParts.String(), ""),`
             },
             {
                 from: `Label:         display.String(),`,
-                to: `Label:         strings.ToValidUTF8(display.String(), ""),`
+                to__: `Label:         strings.ToValidUTF8(display.String(), ""),`
             }
         ]
     },
@@ -33,28 +34,33 @@ const toPatch = [
         replace: [
             {
                 from: `if runtime.GOOS == "windows" {`,
-                to: `if runtime.GOOS == "windows" || runtime.GOOS == "ios" {`
+                to__: `if runtime.GOOS == "windows" || runtime.GOOS == "ios" {`
             }
         ]
     }
-]
+];
 
 function patch() {
-    toPatch.forEach(p => {
+    toPatch.forEach((p) => {
         let contents = fs.readFileSync(p.file, { encoding: "utf8" });
-        p.replace.forEach(({ from, to }) => contents = contents.replace(from, to))
-        fs.writeFileSync(p.file, contents)
-    })
+        p.replace.forEach(({ from, to__ }) => {
+            if (!contents.includes(from) && !contents.includes(to__)) {
+                throw `Cannot find text to replace\n${p.file}\n[${from}]\n[${to__}] `;
+            }
+            contents = contents.replace(from, to__);
+        });
+        fs.writeFileSync(p.file, contents);
+    });
 
     // put tsgo module into the codebase
-    const sourceFile = "./typescript-go-patch/module/tsgo.go";
-    const outDir = "../typescript-go/cmd/module";
-    const outFile = outDir + "/tsgo.go";
+    const sourceFile = path.resolve("typescript-go-patch", "module", "tsgo.go");
+    const outDir = path.resolve("typescript-go", "cmd", "module");
+    const outFile = path.resolve(outDir, "tsgo.go");
 
     fs.mkdirSync(outDir, { recursive: true });
     fs.cpSync(sourceFile, outFile);
 }
 
-if(fs.readdirSync(tsgoDirectory).length > 0) {
+if (fs.readdirSync(tsgoDirectory).length > 0) {
     patch();
 }
