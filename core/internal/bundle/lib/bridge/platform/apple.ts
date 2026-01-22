8417e8cc9cbcaf123009f86bd2b60577c27bfd28
cplepage <lepage.charlesphilippe@gmail.com>
2026-01-22 16:45:27 -0500

    fmt

--- core/internal/bundle/lib/bridge/platform/apple.ts
+++ core/internal/bundle/lib/bridge/platform/apple.ts
@@ -1,14 +1,14 @@
 import { PlatformBridge } from "./index.ts";
-import { fromByteArray,toByteArray } from "../base64.ts";
+import { fromByteArray, toByteArray } from "../base64.ts";
 
-const responsePromises = new Map<number, (response: ArrayBuffer) => void>()
+const responsePromises = new Map<number, (response: ArrayBuffer) => void>();
 
 export function BridgeAppleInit(): PlatformBridge {
-    globalThis.respond = function(id: number, responseB64: string) {
+    globalThis.respond = function (id: number, responseB64: string) {
         const promise = responsePromises.get(id);
         promise?.(toByteArray(responseB64).buffer);
         responsePromises.delete(id);
-    }
+    };
 
     return {
         ctx: globalThis.ctx,
@@ -16,13 +16,13 @@ export function BridgeAppleInit(): PlatformBridge {
             const uint8array = new Uint8Array(payload);
             const id = uint8array[1];
             const base64 = fromByteArray(uint8array);
-            return new Promise<ArrayBuffer>(resolve => {
+            return new Promise<ArrayBuffer>((resolve) => {
                 responsePromises.set(id, resolve);
                 globalThis.webkit.messageHandlers.bridge.postMessage(base64);
             });
         },
         Sync(payload) {
-            throw "sync bridge not yet implemented"
+            throw "sync bridge not yet implemented";
         }
     };
 }
