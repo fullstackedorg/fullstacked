846377cfbed92dc742d0bf2f1efdef2cfaeebe51
cplepage <lepage.charlesphilippe@gmail.com>
2026-08-06 12:37:20 -0400

    needed dynamic import

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 import "./build.ts";
 import "./platform/node/build.ts";
-import "./platform/node/src/index.ts";
+
+// build process updates the version in package.json
+// if already imported, the version get verified before update.
+await import("./platform/node/src/index.ts");
