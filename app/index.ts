f02bbfca56208f25ed63a4bc50796855e5c0389e
cplepage <lepage.charlesphilippe@gmail.com>
2026-09-01 01:11:19 -0400

    expected-keyboard exp

--- app/index.ts
+++ app/index.ts
@@ -11,8 +11,88 @@ const saveSkipWelcomeUntil = (dontShowAgain: boolean) => {
     setConfig("skipWelcomeUntil", until.toString());
 };
 
+const getExpectedKeyboard = async () => {
+    const config = await getConfig("expectedKeyboard");
+    return Boolean(config && config !== "false" && config !== "0");
+};
+
 const openTerminal = async () => {
-    await import("./shell");
+    const isExpectedKeyboard = await getExpectedKeyboard();
+    const initShell = (await import("./shell/init")).default;
+
+    if (isExpectedKeyboard) {
+        const { ExpectedKeyboard, LAYOUT_QWERTY } = await import(
+            "./expected-keyboard/src"
+        );
+
+        let shellInstance: any = null;
+        let fitAddonInstance: any = null;
+
+        const keyboard = new ExpectedKeyboard({
+            layout: LAYOUT_QWERTY,
+            cornerLabelsVisible: true,
+            quickNavVisible: true,
+            swipeDeadzone: 10,
+            autoAttachInputs: true,
+            onInput: (char) => {
+                if (shellInstance) {
+                    if (char === "\n") {
+                        shellInstance.handleInput("\r");
+                    } else {
+                        shellInstance.handleInput(char);
+                    }
+                }
+            },
+            onAction: (action) => {
+                if (shellInstance) {
+                    switch (action) {
+                        case "esc":
+                            shellInstance.handleInput("\x1b");
+                            break;
+                        case "backspace":
+                            shellInstance.handleInput("\u007F");
+                            break;
+                        case "tab":
+                            shellInstance.handleInput("\t");
+                            break;
+                        case "left":
+                            shellInstance.handleInput("\x1b[D");
+                            break;
+                        case "right":
+                            shellInstance.handleInput("\x1b[C");
+                            break;
+                        case "up":
+                            shellInstance.handleInput("\x1b[A");
+                            break;
+                        case "down":
+                            shellInstance.handleInput("\x1b[B");
+                            break;
+                        case "home":
+                            shellInstance.handleInput("\x1b[H");
+                            break;
+                        case "end":
+                            shellInstance.handleInput("\x1b[F");
+                            break;
+                        case "ctrl+c":
+                            shellInstance.handleInput("\u0003");
+                            break;
+                    }
+                }
+            },
+            onVisibilityChange: () => {
+                if (fitAddonInstance) {
+                    setTimeout(() => fitAddonInstance.fit(), 50);
+                    setTimeout(() => fitAddonInstance.fit(), 250);
+                }
+            }
+        });
+
+        const { shell, fitAddon } = await initShell({ keyboard });
+        shellInstance = shell;
+        fitAddonInstance = fitAddon;
+    } else {
+        await initShell();
+    }
 };
 
 const getSkipWelcomeUntil = async () => {
