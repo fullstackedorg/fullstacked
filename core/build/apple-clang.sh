ac2b3e1d237ddbb5538d1f42fe7ec23ad7df9c21
cplepage <lepage.charlesphilippe@gmail.com>
2026-09-16 08:59:45 -0400

    xcode fixes

--- core/build/apple-clang.sh
+++ core/build/apple-clang.sh
@@ -2,13 +2,25 @@
 
 # go/clangwrap.sh
 
+SDK="${SDK:-macosx}"
+
+if [ "$SDK" = "macosx" ]; then
+    unset IPHONEOS_DEPLOYMENT_TARGET
+    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET:-15.0}"
+    MIN_VERSION_FLAG="-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+elif [ "$SDK" = "iphoneos" ]; then
+    unset MACOSX_DEPLOYMENT_TARGET
+    export IPHONEOS_DEPLOYMENT_TARGET="${IPHONEOS_DEPLOYMENT_TARGET:-15.0}"
+    MIN_VERSION_FLAG="-miphoneos-version-min=$IPHONEOS_DEPLOYMENT_TARGET"
+fi
+
 SDK_PATH=`xcrun --sdk $SDK --show-sdk-path`
 CLANG=`xcrun --sdk $SDK --find clang`
 
-if [ "$GOARCH" == "amd64" ]; then
+if [ "$GOARCH" = "amd64" ]; then
     CARCH="x86_64"
-elif [ "$GOARCH" == "arm64" ]; then
+elif [ "$GOARCH" = "arm64" ]; then
     CARCH="arm64"
 fi
 
-exec $CLANG -arch $CARCH -isysroot $SDK_PATH "$@"
+exec $CLANG ${CARCH:+-arch $CARCH} -isysroot "$SDK_PATH" $MIN_VERSION_FLAG "$@"
